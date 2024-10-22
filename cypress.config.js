881f61c027a67b0a497d507a64f85ce0b8be249d
Alexandr Tabacari <atabacarigmail.com>
2024-10-22 15:44:35 +0200

    Added report funtion

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,11 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
-  video: false
-  // reporter: 'cypress-multi-reporters',
-  // reporterOptions: {
-  //   configFile: 'reporter-config.json',
-  // }
+  video: false,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  }
   ,
   e2e: {
     baseUrl: 'http://localhost:4200/',
